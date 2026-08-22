239ba54ab494a0945444cbb210fec20b51ede287
starm12377-lang <starm12377@gmail.com>
2026-08-22 14:44:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 from telethon import TelegramClient, events
 
-# هذه البيانات ستأخذها من "موقع الاستضافة" لاحقاً
+# قراءة البيانات بأمان من السيرفر
 api_id = int(os.environ.get('API_ID', 0))
 api_hash = os.environ.get('API_HASH', '')
-phone = os.environ.get('PHONE', '')
+token = os.environ.get('TOKEN', '')
 
-client = TelegramClient('session_name', api_id, api_hash)
+# تشغيل البوت باستخدام التوكن مباشرة بدون طلب رقم هاتف أو كود
+client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=token)
 
 @client.on(events.NewMessage(pattern='.ping'))
 async def handler(event):
-    await event.reply('سورس البوت يعمل 24/7 بنجاح! ✅')
+    await event.reply("البوت يعمل 24/7 بنجاح ✔️")
 
 async def main():
-    await client.start(phone=phone)
-    print("البوت يعمل الآن...")
+    print("...البوت يعمل الآن...")
     await client.run_until_disconnected()
 
 if __name__ == '__main__':
